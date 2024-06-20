3bb88641273f5064cacc88aee213119920e0f3a1
cipher <dilsurbet@hmail.com>
2024-06-20 15:13:27 +0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1 className="text">Product Information</h1>
+      <h1 className="text">Product Card</h1>
       <ProductCard />
     </div>
   );
